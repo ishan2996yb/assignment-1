b6e685dc52fa059f5d1d4433ce3f40df7f22376e
ishan2996yb <ishandhar2005@gmail.com>
2025-11-17 14:00:47 +0530

    initial commit

---
+++ sumofnumbers.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("The sum is", add)
+print("The subtract is", sub)
+print("The multiplication is", mul)
+print("The division is", div)
